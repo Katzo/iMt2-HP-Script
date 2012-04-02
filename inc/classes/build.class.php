bd36d4e164b4065db9de9988d820da4e2d39d12a
Robin <imer@imer.cc>
2012-04-03 00:30:32 +0200

    Strange bug where content gets overwritten or something?

--- inc/classes/build.class.php
+++ inc/classes/build.class.php
@@ -41,6 +41,7 @@ class build {
 			include($file);
 			if (!isset($content) || !is_array($content)) $this->error("The file ".$file." didnt produce any kind of array :(");
 			$cont[] = $content;
+			var_dump($cont);
 		}
 		include($this->config["path"]["includes"]."/design.inc.php"); // Pass the generated content to design.inc.php for final design echoing
 	}
