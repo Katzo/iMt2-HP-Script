52db582d0f083dd995786a4d2a02f3ca7dcbdaf3
Robin <imer@imer.cc>
2012-04-03 00:31:11 +0200

    Strange bug where content gets overwritten or something?

--- inc/plugins/wbb_news/wbb_news.con.php
+++ inc/plugins/wbb_news/wbb_news.con.php
@@ -13,6 +13,7 @@ if (!isset($db->$plugin_conf["wbbcon"])) $this->error("I could not find the data
 $wbbdb = $plugin_conf["wbbcon"]."db";
 if (!array_key_exists("board",$db->$wbbdb)) $this->error("I could not find the database index board");
 $q = mysql_query("SELECT threadID,topic,firstPostPreview,userID,username,wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_thread.time FROM ".$db->$wbbdb["board"].".wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_thread JOIN ".$db->$wbbdb["board"].".wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_board ON wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_thread.boardID = wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_board.boardID OR wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_thread.boardID = wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_board.parentID WHERE wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_board.parentID = '".$plugin_conf["id"]."' or wbb".$plugin_conf["boardid"]."_".$plugin_conf["installationid"]."_board.boardID = '".$plugin_conf["id"]."' and isDeleted=0 ORDER BY time DESC LIMIT ".$plugin_conf["count"]);
+echo mysql_error();
 $content = array("multi" => true);
 while ($res = mysql_fetch_object($q))
 	$content[] = array(
