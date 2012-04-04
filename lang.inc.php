3474f318129b793dbffab4c4087598f92ff16e49
Robin <imer@imer.cc>
2012-04-05 01:47:25 +0200

    Changes to config and such - Userpanel:Logged in thingy

---
+++ lang.inc.php
@@ -0,0 +1,33 @@
+<?php
+// Lang
+$lang = array(
+	"time" => array(
+		"s" => "seconds",
+		"S" => "second",
+		"m" => "minutes",
+		"M" => "minute",
+		"h" => "hours",
+		"H" => "hour",
+		"a" => "ago",
+		"af" => "a few",
+		"format" => "%value %time %ago",
+		"tformat" => "d.m.y H:i"
+	),
+	"misc" => array(
+		"fillout" => "Please fill out all fields",
+		"coin" => "Coin",
+		"coins" => "Coins",
+		"noacc" => "No account with that password or username found",
+		"pass" => "Password",
+		"user" => "Username",
+		"login" => "Login",
+		"register" => "Register",
+		"forgot" => "Forgot password?",
+		"support" => "Trouble signing in?",
+		"hello" => "Hello <b>%username</b>,",
+		"youcoin" => "You have <b>%coins</b> %coinname!",
+		"donate" => "Donate?",
+	),
+);
+
+?>
