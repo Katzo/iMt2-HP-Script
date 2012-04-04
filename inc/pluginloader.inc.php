833fd80f549e3a70789aeffb8eeeb29bbc3ff838
Robin <imer@imer.cc>
2012-04-04 18:50:10 +0200

    Typos :(

--- inc/pluginloader.inc.php
+++ inc/pluginloader.inc.php
@@ -6,14 +6,15 @@
  * 
  * This file autoloads all .php files in the plugin folder which you configured via $page[$_GET["p"]]["plugins"]
  */
- if (isset($ajax))
+ if (isset($ajax)){
+ 	$filename =$config["path"]["includes"].$config["path"]["plugins"].$ajax[$p].".php";
+	include($filename);
+
+	 }
+else {
 	 foreach ($pages[$p]["plugins"] as $file) { 
 	    $filename =$config["path"]["includes"].$config["path"]["plugins"]. $file.".php";
 	    include($filename);
-	 }
-else {
-	$filename =$config["path"]["includes"].$config["path"]["plugins"].$ajax[$p].".php";
-	include($filename);
 }
 	
  
