d3c729ba6be3e678c617452406bc232ca9e2bf69
Robin <imer@imer.cc>
2012-04-04 18:42:20 +0200

    Typos :D

--- ajax.php
+++ ajax.php
@@ -4,7 +4,7 @@ if (!session_id())
 include("config.inc.php");
 include($config["path"]["includes"]."common.inc.php");
 if (!isset($ajax[$p])) e404();
-$db = new Database;
+$db = new database;
 $ajax = true;
 include($config["path"]["includes"]."pluginloader.inc.php");
 ?>
