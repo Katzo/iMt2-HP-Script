7e36bf2c7e87295f99c390ef5fc9e4b7806ad43b
Robin <imer@imer.cc>
2012-04-05 16:54:40 +0200

    Forgot to include the lang file for ajax requests. thats usefull...

--- ajax.php
+++ ajax.php
@@ -2,6 +2,7 @@
 if (!session_id())
 	session_start();
 include("config.inc.php");
+include("lang.inc.php");
 include($config["path"]["includes"]."common.inc.php");
 if (!isset($ajax[$p])) e404();
 $db = new database;
