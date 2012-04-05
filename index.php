ebdd3088f8a9b8991471a20892c6e4d2332ea977
Robin <imer@imer.cc>
2012-04-05 14:16:03 +0200

    Allow plugins to modify the navi before it gets added to the build list

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ include("lang.inc.php");
 include($config["path"]["includes"]."common.inc.php");
 if (!isset($pages[$p])) e404();
 $build = new build;
+include($config["path"]["includes"]."pluginloader.inc.php");
 foreach ($navilinks["header"] as $n)
 	$build->addNavi($n);
 foreach ($navilinks["footer"] as $n)
 	$build->addFooter($n);
-
-include($config["path"]["includes"]."pluginloader.inc.php");
 $build->build();
 ?>
