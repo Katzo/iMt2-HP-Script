2021689171cefe9cbfcafa737c638a56450f7ef9
Robin <imer@imer.cc>
2012-04-05 15:22:54 +0200

    Random stuff i dont remember

--- inc/plugins/register/register.con.php
+++ inc/plugins/register/register.con.php
@@ -10,14 +10,27 @@
 include($config["path"]["includes"].$config["path"]["plugins"]."register/config.inc.php");
 // Note to self: https://developers.google.com/recaptcha/docs/php for captcha
 if ($plugin_conf["enabled"]){
-	
+	if (isset($_POST["submit"])){
+		// Reg here
+	}else{
+		$content = array(
+			"head" => array(
+				"title" => $lang["misc"]["register"],
+			),
+			"middle" => array(
+				"text" => '<form method="POST"><table>
+							<tr><td>'.$lang["misc"]["user"].'</td><td><input type="text" placeholder="'.$lang["misc"]["user"].'"/></td><td id="ures"></td></tr>
+						</table></form>',
+			)
+		);
+	}
 }else
 	$content = array(
 		"head" => array(
 			"title" => $lang["misc"]["register"],
 		),
 		"middle" => array(
-			"text" => ""
+			"text" => $lang["misc"]["register_closed"],
 		)
 	);
 unset($plugin_conf);
