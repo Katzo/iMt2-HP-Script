031a8ee453213ffb470f9086679b0b671088db75
Robin <imer@imer.cc>
2012-04-07 19:22:57 +0200

    On the fly cache building for ranking improvements

--- inc/plugins/ranking/buildcache.inc.php
+++ inc/plugins/ranking/buildcache.inc.php
@@ -6,13 +6,21 @@
     }
     elseif (!mysql_errno()) // bla code
     //table exist*/
-$q = mysql_query("SELECT id,name,level,job FROM ".$db->gamedb["player"].".player ORDER BY level DESC,exp DESC",$db->game); // do fancy join ranking_cache and check for time. it'll be overly complicated for split connections :/
+if (isset($db->hp))
+	$q = mysql_query("SELECT id,name,level,job FROM ".$db->gamedb["player"].".player ORDER BY level DESC,exp DESC",$db->game); // can't do this with joins :/
+else
+	$q = mysql_query("SELECT player.id,player.name,player.level,player.job FROM ".$db->gamedb["player"].".player JOIN ".$db->gamedb["homepage"].".ranking_cache ON player.id=ranking_cache.id WHERE ".time().">time-".$plugin_conf["cachetimeout"]." ORDER BY level DESC,exp DESC",$db->game);
 $i=0; // General
 $wi=0; // Warrior
 $ai=0; // Assasin (spelling..)
 $sui=0; // Sura
 $sai=0; // Shaman
 while ($res = mysql_fetch_object($q)) {
+	if (isset($db->hp)) {
+		$c = mysql_fetch_object(mysql_query("SELECT time FROM ".$db->hpdb["homepage"].".ranking_cache WHERE id='".$res->id."'"));
+		if (time() < $c->time-$plugin_cache["cachetimeout"])
+			continue;
+	}
 	$i++;
 	if ($res->job == 0 || $res->job == 4) {
 		$wi++;
