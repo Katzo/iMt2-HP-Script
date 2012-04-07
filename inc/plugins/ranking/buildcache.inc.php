db0e402bfa47f62146a549298d9f83eefd6321f7
Robin <imer@imer.cc>
2012-04-07 22:54:29 +0200

    Cache fix

--- inc/plugins/ranking/buildcache.inc.php
+++ inc/plugins/ranking/buildcache.inc.php
@@ -7,21 +7,19 @@
     elseif (!mysql_errno()) // bla code
     //table exist*/
 echo "<!-- Building ranking cache! -->";
-if (isset($db->hp))
-	$q = mysql_query("SELECT id,name,level,job FROM ".$db->gamedb["player"].".player ORDER BY level DESC,exp DESC",$db->game); // can't do this with joins :/
-else
-	$q = mysql_query("SELECT player.id,player.name,player.level,player.job FROM ".$db->gamedb["player"].".player JOIN ".$db->gamedb["homepage"].".ranking_cache ON player.id=ranking_cache.id WHERE ".time().">time-".$plugin_conf["cachetimeout"]." ORDER BY level DESC,exp DESC",$db->game);
+$q = mysql_query("SELECT id,name,level,job FROM ".$db->gamedb["player"].".player ORDER BY level DESC,exp DESC",$db->game); 
 $i=0; // General
 $wi=0; // Warrior
 $ai=0; // Assasin (spelling..)
 $sui=0; // Sura
 $sai=0; // Shaman
 while ($res = mysql_fetch_object($q)) {
-	if (isset($db->hp)) {
-		$c = mysql_fetch_object(mysql_query("SELECT time FROM ".$db->hpdb["homepage"].".ranking_cache WHERE id='".$res->id."'"));
-		if (time() < $c->time-$plugin_cache["cachetimeout"])
-			continue;
-	}
+	if (isset($db->hp)) 
+		$c = mysql_fetch_object(mysql_query("SELECT time FROM ".$db->hpdb["homepage"].".ranking_cache WHERE id='".$res->id."'",$db->hp));
+	else
+		$c = mysql_fetch_object(mysql_query("SELECT time FROM ".$db->gamedb["homepage"].".ranking_cache WHERE id='".$res->id."'",$db->hp));
+	if (time() < $c->time+$plugin_cache["cachetimeout"]*1.5)
+		continue;
 	$i++;
 	if ($res->job == 0 || $res->job == 4) {
 		$wi++;
