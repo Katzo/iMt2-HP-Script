c41e7242698eb168d0785cba909b1a4d82dc3c1e
Robin <imer@imer.cc>
2012-04-07 23:05:36 +0200

    Ranking: dont show staff members

--- inc/plugins/ranking/buildcache.inc.php
+++ inc/plugins/ranking/buildcache.inc.php
@@ -8,6 +8,7 @@
     //table exist*/
 echo "<!-- Building ranking cache! -->";
 $q = mysql_query("SELECT player.id,player.name,player.level,player.job FROM ".$db->gamedb["player"].".player LEFT JOIN ".$db->gamedb["common"].".gmlist ON gmlist.mName = player.name WHERE gmlist.mName is null AND account.status = 'OK' ORDER BY level DESC,exp DESC",$db->game); 
+echo mysql_error($db->game);
 $i=0; // General
 $wi=0; // Warrior
 $ai=0; // Assasin (spelling..)
