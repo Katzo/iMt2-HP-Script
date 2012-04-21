ce9b614afdfbd126074594df4be3a60ba323059f
Robin <imer@imer.cc>
2012-04-21 21:24:02 +0200

    Added basic error catching and reporting

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 if (!session_id())
 	session_start();
 try{
-include("config.inc.php");
-include("lang.inc.php");
-include($config["path"]["includes"]."common.inc.php");
-if (!isset($pages[$p])) e404();
-$build = new build;
-include($config["path"]["includes"]."pluginloader.inc.php");
-foreach ($navilinks["header"] as $n)
-	$build->addNavi($n);
-foreach ($navilinks["footer"] as $n)
-	$build->addFooter($n);
-$build->build();
-}
-catch(Exception $e){
-	new Error($e->__toString());
+	try{
+		include("config.inc.php");
+		include("lang.inc.php");
+		include($config["path"]["includes"]."common.inc.php");
+		if (!isset($pages[$p])) e404();
+		$build = new build;
+		include($config["path"]["includes"]."pluginloader.inc.php");
+		foreach ($navilinks["header"] as $n)
+			$build->addNavi($n);
+		foreach ($navilinks["footer"] as $n)
+			$build->addFooter($n);
+		$build->build();
+	}
+	catch(Exception $e){
+		new Error($e->__toString());
+	}
 }
 catch(ErrorException $e){
 	new Error($e->__toString());
