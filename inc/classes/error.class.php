fba27836259057e28a81dff59a2440cfedd65db9
Robin <imer@imer.cc>
2012-04-21 21:28:03 +0200

    Added basic error catching and reporting

--- inc/classes/error.class.php
+++ inc/classes/error.class.php
@@ -1,6 +1,7 @@
 <?php
-class Error {
+class error {
 	public function __construct($content = "I made a mistake! :(<br/>I am so sorry this happened<br/>Please try again and contact someone when the problem persists!<br/><br/>Bye~",$title = "Help!"){
+		$content = str_replace("\n","<br/>",$content);
 		die('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
