f952a7aa97a9b391074c3dece8291eca5248551a
Robin <imer@imer.cc>
2012-04-22 20:38:01 +0200

    Better URLs: Image path is now absolute

--- inc/common.inc.php
+++ inc/common.inc.php
@@ -28,6 +28,9 @@ function e404() {
 	header("Status: 404 Not Found");
 	die("<h1>I made a mistake! Help! :(</h1>I could not find the page you were looking for... :(");
 }
+/*
+ * time functions
+ */
 function timetostr($timestamp) {
 	global $lang;
 	if (!$timestamp) return false;
@@ -46,9 +49,15 @@ function timetostr($timestamp) {
 	if ($hours < 24)
 		return timeformat($hours,$lang["time"]["h"]);
 	return date($lang["time"]["tformat"],$timestamp);
- }
- function timeformat($value,$time) {
- 	global $lang;
- 	return str_replace("%ago",$lang["time"]["a"],str_replace("%time",$time,str_replace("%value",$value,$lang["time"]["format"])));
- }
+}
+function timeformat($value,$time) {
+	global $lang;
+	return str_replace("%ago",$lang["time"]["a"],str_replace("%time",$time,str_replace("%value",$value,$lang["time"]["format"])));
+}
+/*
+ * Function for checking on valid URL (used for images)
+ */ 
+function isURL($url){
+	return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
+}
 ?>
